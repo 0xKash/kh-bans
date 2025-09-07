f8505ebd026a6071ee24a810aac32a855ef31b8d
kash <wwwkash2@gmail.com>
2025-09-08 00:03:49 +0200

    feat: create ban command for console and admin players

--- server/server.lua
+++ server/server.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: param-type-mismatch
 AddEventHandler("playerConnecting", function (name, setKickReason, deferrals)
     local src = source
     deferrals.defer()
@@ -31,3 +32,68 @@ AddEventHandler("playerConnecting", function (name, setKickReason, deferrals)
 
     end, 'POST', json.encode(identifiers), { ['Content-Type'] = 'application/json' })
 end)
+
+RegisterCommand("kh-ban", function (source, args)
+    local src = source
+
+    if src ~= 0 and not IsPlayerAceAllowed(src, "admin") then
+        TriggerClientEvent("chat:addMessage", src, { args = { "^1SYSTEM", "You do not have permission" } })
+        return
+    end
+
+    local targetId = tonumber(args[1])
+    local reason = table.concat(args, " ", 2)
+    local expiresAt
+
+    if not targetId  then
+        if src ~= 0 then
+            TriggerClientEvent("chat:addMessage", src, { args = { "^1SYSTEM", "Usage: /ban [playerId] [reason]" } })
+        else
+            print("^1SYSTEM: Usage: /ban [playerId] [reason]")
+            
+        end
+        return
+    end
+
+    if not GetPlayerName(targetId)  then
+        if src ~= 0 then
+            TriggerClientEvent("chat:addMessage", src, { args = { "^1SYSTEM", "Invalid player ID" } })
+        else
+            print("^1SYSTEM: Invalid player ID")
+        end
+        return
+    end
+
+    local identifiers = Utils.getPlayerIdentifiersObject(targetId)
+    local body = {
+        reason = reason or "No reason provided",
+        expiresAt = expiresAt or nil
+    }
+
+    for key, value in pairs(identifiers) do
+        body[key] = value
+    end
+
+    PerformHttpRequest("http://localhost:3000/bans", function (status, response)
+        if status == 201 then
+            local banMessage = "You have been banned: " .. body.reason
+            if body.expiresAt then
+                banMessage = banMessage .. " (expires at " .. body.expiresAt .. ")"
+            end
+
+            DropPlayer(targetId, banMessage)
+
+            if src ~= 0 then
+                TriggerClientEvent("chat:addMessage", src, { args = { "^2SYSTEM", "Player banned successfully" } })
+            else
+                print("^2SYSTEM:", "Player banned successfully")
+            end            
+        else
+            if src ~= 0 then
+                TriggerClientEvent("chat:addMessage", src, { args = { "^1SYSTEM", "Error banning player: " .. response } })
+            else
+                print("^1SYSTEM: Error banning player: " .. tostring(response))
+            end
+        end
+    end, 'POST', json.encode(body), { ["Content-Type"] = "application/json" })
+end, false)
